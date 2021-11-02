1a1a3aac9111b3c4a8b0c36a2bed794038796aa8
nikitha chimmmula <nikithachimmula97@gmail.com>
2021-11-02 16:11:30 +0530

    regex commit

---
+++ MyBinaryTreeTest.java
@@ -0,0 +1,16 @@
+
+public class MyBinaryTreeTest {
+
+	public void addingThreeNumbersToBinaryTree() {
+		MyBinaryTree<Integer> myBinaryTree = new MyBinaryTree<>();
+		myBinaryTree.add(56);
+		myBinaryTree.add(30);
+		myBinaryTree.add(70);
+		int size = myBinaryTree.getSize();
+
+	}
+	public static void main(String[] args) {
+		
+	}
+
+}
